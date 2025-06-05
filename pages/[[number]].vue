d0294ac8f2061d8b2e565ffe061b370be0e178ca
eduair94 <49965850+eduair94@users.noreply.github.com>
2025-06-04 23:50:01 -0300

    fix: number input delete

--- pages/[[number]].vue
+++ pages/[[number]].vue
@@ -209,6 +209,8 @@ const phoneApi = usePhoneApi({
 const loading = computed(() => localLoading.value || phoneApi.loading.value);
 
 const phoneValid = computed(() => {
+  if (!phoneCode.value) return false;
+  if (!phoneNumber.value) return false;
   const phoneNumberFull = phoneCode.value.code + phoneNumber.value.trim();
   const phone = parsePhoneNumber("+" + phoneNumberFull);
   if (!phone) return false;
