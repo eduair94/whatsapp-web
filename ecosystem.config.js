34d9cc3fd15a44c54ba923212013af8117e2a6da
eduair94 <49965850+eduair94@users.noreply.github.com>
2025-06-26 01:48:41 -0300

    Fix max memory restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'WP_Checker',
       port: '3624',
-      max_memory_restart: '200M',
+      max_memory_restart: '1G',
       exec_mode: 'cluster',
       instances: '1',
       script: './.output/server/index.mjs'
